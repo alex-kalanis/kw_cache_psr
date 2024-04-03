2c97ebc73e57138f055ecf5a6bc7f0af428019f6
Petr Plsek <me@kalanys.com>
2024-04-04 00:06:24 +0200

    kw_cache_psr - dependency analyzer found

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -13,5 +13,6 @@ $config = new Configuration();
 return $config
     // ignore errors on specific packages and paths
     ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_files', __DIR__ . '/php-src/Adapters/FilesAdapter.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
+    ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_paths', __DIR__ . '/php-src/Adapters/FilesAdapter.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
     ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_storage', __DIR__ . '/php-src/Adapters/StorageAdapter.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
 ;
